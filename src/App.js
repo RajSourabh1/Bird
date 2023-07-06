ca29f637c054dcc73d9415114901b1d5a1a92ecf
Sourabh <createownpath111@gmail.com>
2023-07-06 23:42:26 +0530

    wrong on same input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+const [bird,setBird] = useState({});
+const [birds,setBirds] = useState([]);
+
+const add = () => {
+  setBirds([...birds,bird]);
 }
 
-export default App;
+const like = (id)=>{
+   var myObj = birds[id];
+  //  console.log("Arr before like",myObj)
+   myObj.like += 1;
+  //  console.log("Arr after like",myObj)
+   var newArr = birds.map((item,i)=>i==id?myObj:item)
+   setBirds(newArr); 
+}
+
+return(
+  <div>
+     <input type="text" onChange={e=>setBird({panchi:e.target.value,like:0})}/>
+     <button onClick={add}>Add</button>
+     <ul>
+        {
+          birds.map((value,index)=>(
+            <li key={index}>
+              <span>{value.panchi}</span>
+              <button onClick={()=>like(index)}>{value.like}</button>
+            </li>
+          ))
+        }
+     </ul>
+  </div>
+)
+}
+
+export default App
